8899d91131eaa908b28fec478c35b1e76a1e2982
EdigleisonBr <edigleisonbr@gmail.com>
2022-03-23 12:16:08 -0300

    add api in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: FirstComponentComponent},
+    {path: 'list', component: ListRenderComponent},
+    {path: 'list/:id', component: ItemDetailComponent},
+    {path: 'event', component: EventosComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+    username = 'Edigleison';
+
+    userData = {
+        email: 'edigleisonbr@gmail.com',
+        role: 'Admin',
+    };
+
+    title = 'curso-angular';
 }
